4a83674076b06d670b544edbfaa5d5e7fddfa17e
soulhyeok <dat.sci.seol@gmail.com>
2020-02-15 09:38:52 +0900

    solved

--- basic/10828/stack.py
+++ basic/10828/stack.py
@@ -1,1 +1,24 @@
-sdf
+
+opers = int(input().rstrip('\n'))
+stack = []
+
+for i in range(opers):
+    cmd = input().rstrip('\n')
+    if (cmd == "empty"):
+        print(int(len(stack) == 0))
+    elif (cmd == "size"):
+        print(len(stack))
+    elif (cmd == "top"):
+        if (len(stack) == 0):
+            print("-1")
+        else:
+            print(stack[len(stack) - 1])
+    elif (cmd.split()[0] == "push"):
+        stack.append(int(cmd.split()[1]))
+    elif (cmd == "pop"):
+        if (len(stack)):
+            print(stack[len(stack) - 1])
+            del stack[len(stack) - 1]
+
+        else:
+            print("-1")
